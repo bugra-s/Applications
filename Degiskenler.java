60d67afae7a5e5eb69906a9807dfa9a75300f957
b.Hawk <81382952+bugra-s@users.noreply.github.com>
2023-01-16 20:31:37 +0300

    Add files via upload

---
+++ Degiskenler.java
@@ -0,0 +1,18 @@
+package com.example.note52;
+
+public class Degiskenler {
+
+    public static void main(String args[]){
+
+        String kisininAdi = "Buğra";
+        int kisininYasi = 14;
+        double kisininBoyu = 1.72;
+        boolean turkMu = true ;
+        int a = 10;
+        int b = 50;
+
+         System.out.println(a+b);
+
+    }
+
+}
